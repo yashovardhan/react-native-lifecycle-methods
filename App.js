fecbecefafbca71608b5cfc7b6e892c660e61afa
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-04-03 14:09:23 +0530

    App.js: Complete Counter

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor called.");
+    this.state = {
+      number: 0,
+      mountButtonTitle: 'UNMOUNT',
+    }
+    this.showCounter = true;
+  };
+
+  incrementNumber() {
+    this.setState({number: this.state.number + 1})
+  }
+
+  toggleCounter() {
+    if(this.showCounter === true){
+      this.setState({mountButtonTitle: 'MOUNT'});
+      this.showCounter = false;
+      this.setState({number: 0});
+    }
+    else {
+      this.setState({mountButtonTitle: 'UNMOUNT'});
+      this.showCounter = true;
+    }
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount called.");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount called.");
+  }
+
+  componentWillReceiveProps(nextProp) {
+    console.log("componentWillReceiveProps called.");
+  }
+
+  shouldComponentUpdate(nextProp, nextState) {
+    console.log("shouldComponentUpdate called.");
+    return true;
+  }
+
+  componentWillUpdate(nextProp, nextState) {
+    console.log("componentWillUpdate called.");
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+    console.log("componentDidUpdate called.");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount called.");
+  }
+
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch called.");
+    //Handle error.
+  }
+
   render() {
+    console.log("render called");
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {this.showCounter ? <Text style={styles.text}>{this.state.number}</Text> : null}
+        {this.showCounter ? <Button onPress={this.incrementNumber.bind(this)} title="INCREMENT"/> : null}
+        <Button onPress={this.toggleCounter.bind(this)} title={this.state.mountButtonTitle}/>
       </View>
     );
   }
@@ -18,4 +81,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 31,
+    marginBottom: 10,
+  }
 });
